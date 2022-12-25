555cef8a422b588fb8043d1fded62c30bd1ea70b
Artem Baguinski <abaguinski@gmail.com>
2022-12-25 12:00:14 +0100

    Install dotfiles with a script

---
+++ .local/bin/setup-dotfiles.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+
+set -euo pipefail
+
+dotfiles() {
+  git "--git-dir=$HOME/.config/dotfiles/.git" "--work-tree=$HOME" "$@"
+}
+
+dotfiles config --local status.showUntrackedFiles no
+dotfiles checkout
